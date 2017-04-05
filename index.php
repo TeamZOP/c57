60551b19c703b11bd77ce78955159d89a9210377
Lionel COTE <grcote7@gmail.com>
2017-04-05 16:28:39 +0200

    Init

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Accueil | c57.fr</title>
+  <meta charset=UTF-8"/>
+  <meta name="description" content="Page d'accueil de c57.fr"/>
+  <style type="text/css">
+    body {
+      background-color: cornsilk;
+      color: #777777;
+      font-family: arial;
+      text-align: center;
+    }
+
+    h1 {
+      font-size: 64px;
+      color: #555555;
+      margin: 70px 0 50px 0;
+    }
+
+    p {
+      width: 77%;
+      text-align: center;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 30px;
+      font-size: 1.2em;
+    }
+
+    div {
+      width: 77%;
+      text-align: center;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    a {
+      -webkit-text-decoration: none;
+      -moz-text-decoration: none;
+      -ms-text-decoration: none;
+      -o-text-decoration: none;
+      text-decoration: none;
+    }
+
+    a:link {
+      color: #34536A;
+    }
+
+    a:visited {
+      color: #34536A;
+    }
+
+    a:active {
+      color: #34536A;
+    }
+
+    a:hover {
+      color: red;
+      background-color: yellow;
+    }
+
+    .ext a:hover {
+      color: red;
+      background-color: cyan;
+    }
+  </style>
+</head>
+
+<body>
+
+<h1>C57.fr</h1>
+<h3><em>Les OS, c'est en 5 à 7 !</em></h3>
+
+<p>
+  <a href="http://laravel.c57.fr">Laravel</a> |
+  <a href="http://bestmomo.c57.fr">Laravel (Customisé/BestMomo)</a> |
+  <a href="http://october.c57.fr">OctoberCMS</a>
+</p>
+<hr>
+<!--    <p>--><? //='Ce texte est issu d\'un script écrit en PHP '.PHP_VERSION;?><!--</p>-->
+
+<div style="text-align: right" class="ext">
+  &copy; 2017 - Propulsé par <strong><a href="git@github.com:c57fr/c57.git" title="Dépôt GitHub">C57</a></strong> - Initié par
+  <a href="http://fr.lmgtfy.com/?q=GrCOTE7" target="_blank">GrCOTE7</a>
+</div>
+
+</body>
+
+</html>
+
